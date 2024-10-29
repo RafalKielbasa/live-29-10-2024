8e1bac614ed7eba19decbfd8e5159ad42573b63e
Stan <stanislawpera94@gmail.com>
2024-10-29 22:29:26 +0100

    feat

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Typography from '@mui/material/Typography'
+import Table from './components/Table'
 
 function App() {
   return (
     <div>
       <Typography variant='h1' gutterBottom>
         Start here
+      <Table/>
       </Typography>
     </div>
   )
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,45 @@
-const Table = (data) => {
-  return <div>Table</div>
-}
+import MuiTable from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TableSortLabel from "@mui/material/TableSortLabel";
 
-export default Table
+import { products } from "../data";
+
+const headerCells = Object.keys(products[0]);
+console.log(headerCells);
+
+const Table = () => {
+  return (
+    <div>
+      <TableContainer>
+        <MuiTable>
+          <TableHead>
+            <TableRow>
+              {headerCells.map((name, index) => (
+                <TableCell key={index}>{name}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((item) => {
+              const cellValue = Object.values(item);
+              return (
+                <TableRow key={item.id}>
+                  {cellValue.map((cell) => (
+                    <TableCell>{String(cell)}</TableCell>
+                  ))}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </MuiTable>
+      </TableContainer>
+    </div>
+  );
+};
+
+export default Table;
